75eb3a96e354fdd2ea80d098ef8066d62654c905
yasmymstrelow <159436297+yasmymstrelow@users.noreply.github.com>
2024-09-25 19:37:52 -0300

    yasm

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,56 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'português'
+    ' o que é gênero textual?'
+    'são as variadas formas que as pessoas e as organizaçoẽs utilizada para comunicar',
+    
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'cite um pais no sul americano?',
+    'America do sul'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'historia',
+    'ano da guerra fria?',
+    'entre 194 educação financeira é a área que ensina a lidar melhor com suas finanças. 7 e 1991'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'como faklar 18 em inglês?',
+    'eighteen'
 )
 
 criaCartao(
-    'Nova Pergunta',
-    'Isso é um teste?',
-    'SIM'
+    'matematica',
+    '5x9?',
+    '45'
+)
+criaCartao(
+    'fisica'
+    'quem foi o primeiro filosofo'
+    'tales de mileto'
+)
+criaCartao(
+    'educação financeira'
+    'o que é educação financeira'
+    ' educação financeira é a área que ensina a lidar melhor com suas finanças.'
+)
+criaCartao(
+    'ciencias'
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?'
+    'Tem entre 4 a 6 litros. São retirados 450 mililitros'
+)
+criaCartao(
+    'biologia'
+    'As membranas que apresentam as funções de isolar e proteger o Sistema Nervoso Central são chamadas de:'
+    'Meninges'
+
+)
+criaCartao(
+    'quimica'
+    'apresenta um par de isômeros planos'
+    '3-metil-pentano e 2,3-dimetil-butano'
 )
